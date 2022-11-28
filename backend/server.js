71bc71f0d725d58831122f81de775a8767947b69
Alessandro <c2063362@hallam.shu.ac.uk>
2022-11-28 12:41:42 +0000

    Preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,22 +5,30 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const recipeRoutes = require("./routes/recipeRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 const app = express();
 dotenv.config();
 connectDB();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running..");
-});
-
-//app.get("/api/recipes", (req, res) => {
-//  res.json(recipes);
-//});
-
 app.use("/api/users", userRoutes);
 app.use("/api/recipes", recipeRoutes);
+
+//DEPLOYMENT
+
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
